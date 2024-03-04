b8bc8745dabf17754e4a94df3425b3b9f33c6e71
SengokuGarp <garpirungu@gmail.com>
2024-03-04 22:21:36 +0300

    Initial commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+class Dog():
+    def __init__(self, name, breed=None):
+        self.name = name
+        if breed is None:
+           self.breed = "Mutt"
+        else:
+            self.breed = breed
 
-class Dog:
-    pass
+
+
